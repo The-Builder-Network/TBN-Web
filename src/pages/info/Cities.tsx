c1b78e975dc109ddf6cdec94f14dbf79a0fd8e26
Sarthak S Kumar <sarthakskumar7@gmail.com>
2026-03-08 15:13:06 +0530

    feat(ui): enhance build configuration to improve user experience

--- src/pages/info/Cities.tsx
+++ src/pages/info/Cities.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { cities } from "@/data/cities";
+import { cities } from "@/constants/cities";
 import CTASection from "@/components/home/sections/CTASection";
 
 const Cities = () => {
