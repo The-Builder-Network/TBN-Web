6c713231cbf701e87ae7989c749bd2a0a8dc7bc5
Sarthak S Kumar <sarthakskumar7@gmail.com>
2026-04-09 19:43:28 +0530

    chore(old): clean up Header, eslint and next in old (8 files)

--- old/postcss.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('postcss-load-config').Config} */
-const config = {
-  plugins: {
-    "@tailwindcss/postcss": {}, // Use the correct package
-    autoprefixer: {},
-  },
-};
-
-module.exports = config;
